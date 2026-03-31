967fd5df19c4583114456ea2672064aae2084047
MadhurNehete <132289905+MadhurNehete@users.noreply.github.com>
2026-03-31 11:10:25 +0530

    Initial-commit9

--- integrationTests/relayers/slowTests/framework/dharitriHandler.go
+++ integrationTests/relayers/slowTests/framework/dharitriHandler.go
@@ -143,7 +143,7 @@ func (handler *DharitriHandler) deployContracts(ctx context.Context, chainType C
 	// deploy aggregator
 	stakeValue, _ := big.NewInt(0).SetString(minRelayerStake, 10)
 	aggregatorDeployParams := []string{
-		"", // empty = native REWA for RewaOrDcdtTokenIdentifier
+		hex.EncodeToString([]byte("REWA")), // REWA token identifier for staking
 		hex.EncodeToString(stakeValue.Bytes()),
 		"01",
 		"02",
