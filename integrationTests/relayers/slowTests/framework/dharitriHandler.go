577cf779328d1d2f4851a25bdeaa9fd80c243ab4
MadhurNehete <132289905+MadhurNehete@users.noreply.github.com>
2026-04-03 14:47:00 +0530

    stake-value-check

--- integrationTests/relayers/slowTests/framework/dharitriHandler.go
+++ integrationTests/relayers/slowTests/framework/dharitriHandler.go
@@ -521,15 +521,22 @@ func (handler *DharitriHandler) PauseContractsForTokenChanges(ctx context.Contex
 }
 
 func (handler *DharitriHandler) stakeAddressesOnContract(ctx context.Context, contract *DrtAddress, allKeys []KeysHolder) {
+	stakeValue := big.NewInt(0)
+	stakeValue.SetString("1000000000000000000", 10) // 1 token (example)
 	for _, keys := range allKeys {
-		hash, txResult := handler.ChainSimulator.SendTx(
-			ctx,
-			keys.DrtSk,
-			contract,
-			minRelayerStake,
-			setCallsGasLimit,
-			[]byte(stakeFunction),
-		)
+		hash, txResult := handler.ChainSimulator.ScCall(
+    ctx,
+    keys.DrtSk,
+    contract,
+    "0", // no native value
+    setCallsGasLimit,
+    dcdtTransferFunction,
+    []string{
+        hex.EncodeToString([]byte("REWA")),   // token
+        hex.EncodeToString(stakeValue.Bytes()), // amount
+        hex.EncodeToString([]byte("stake")),  // endpoint
+    },
+)
 		log.Info(fmt.Sprintf("Address %s staked on contract %s with transaction hash %s, status %s", keys.DrtAddress, contract, hash, txResult.Status))
 	}
 }
